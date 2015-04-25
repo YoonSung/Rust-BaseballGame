9673efe57c6a6904b124638427670f66ec7f64f8
Yoonsung <lvev9925@naver.com>
2015-04-25 21:29:16 +0900

    Get 3 digit from shell

--- base_ball_game/src/main.rs
+++ base_ball_game/src/main.rs
@@ -1,11 +1,39 @@
 use std::io;
 use std::io::Write;
 
-fn main() {
-    let mut input = String::new();
-    print!("Input Baseball number : ");
-    io::stdout().flush();
 
-    io::stdin().read_line(&mut input);
-    print!("input data : {}", input);
+struct SBO {
+    strike: i8,
+    ball  : i8,
+    out   : i8
+}
+
+fn main() {
+
+    println!("========================");
+    println!("       숫자 야구게임       ");
+    println!("========================");
+
+    let mut inputs = vec![];
+
+    loop {
+
+        let current_input_size = inputs.len();
+
+        let mut input = String::new();
+        print!("{}번째 숫자를 입력해주세요: ", current_input_size+1);
+        io::stdout().flush();
+
+        io::stdin().read_line(&mut input);
+        print!("input data : {}", input);
+//TODO input data validation
+        inputs.push(input);
+
+        if current_input_size == 2{
+            break;
+        }
+    }
+
+    print!("빠져나왔습니다");
+
 }
