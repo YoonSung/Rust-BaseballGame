f446c67123204eb7009b5f1f0964d5f3a040ce45
Yoonsung <lvev9925@naver.com>
2015-04-26 12:23:46 +0900

    Modulization

--- base_ball_game/src/main.rs
+++ base_ball_game/src/main.rs
@@ -15,23 +15,38 @@ fn main() {
     println!("       숫자 야구게임       ");
     println!("========================");
 
-    
     let mut inputs:Vec<i8> = vec![];
     let mut answers:Vec<i8> = vec![]; 
-    let mut sbo = SBO{strike:0, ball:0, out:0};
 
-//TODO Modulization
-//TODO remove overlapped value in answers
     let between = Range::new(1i8, 10);
+    gen_rand_array(&mut answers, 3, between); 
+    inputs = gen_array_from_user(3);
+
+    //Check input data
+    let sbo = get_sbo_result(&inputs, &answers);
+
+    println!("correct answer : {}. {}. {}", answers[0], answers[1],answers[2]);
+    println!("\nGame result\n-----------  \ns : {}\nb : {}\no : {}", sbo.strike, sbo.ball, sbo.out);
+
+}
+
+//TODO remove overlapped value in answers
+fn gen_rand_array(arr: &mut Vec<i8>, len: i8, range: Range<i8>) {
     let mut rng = rand::thread_rng();
-    for i in 0..3 {
-        answers.push(between.ind_sample(&mut rng));
+
+    for i in 0..len {
+        (*arr).push(range.ind_sample(&mut rng));
     }
 
+    
+}
+
+//TODO input data validation
+fn gen_array_from_user(len: usize) -> Vec<i8> {
+    let mut return_arr:Vec<i8> = vec![];
 
     loop {
-//TODO Modulization
-        let current_input_size = inputs.len();
+        let current_input_size = return_arr.len();
 
         let mut input = String::new();
         print!("{}번째 숫자를 입력해주세요: ", current_input_size+1);
@@ -40,29 +55,32 @@ fn main() {
         io::stdin().read_line(&mut input);
         let int_value: Option<i8> = input.trim().parse().ok();
 
-//TODO input data validation
         match int_value {
             Some(value) => {
-                inputs.push(value);
+                return_arr.push(value);
             },
             None => continue
         }
 
-        if current_input_size == 2{
+        if current_input_size == len-1 {
             break;
         }
     }
 
-//Check input data
+    return_arr 
+}
+
 //TODO Modulization
+fn get_sbo_result(inputs :&Vec<i8>, answers :&Vec<i8>) -> SBO {
+    let mut sbo = SBO{strike:0, ball:0, out:0};
     let mut is_heat = false;
     let mut answer_value: i8 = 0;
     let mut input_value: i8 = 0;
-    for answer_index in 0..answers.len() {
-        answer_value = answers[answer_index];
+    for answer_index in 0..(*answers).len() {
+        answer_value = (*answers)[answer_index];
         
-        for input_index in 0..inputs.len() {
-            input_value = inputs[input_index];
+        for input_index in 0..(*inputs).len() {
+            input_value = (*inputs)[input_index];
             if answer_value == input_value {
                 if answer_index == input_index {
                     sbo.strike += 1;
@@ -78,7 +96,6 @@ fn main() {
             sbo.out += 1;
         }
     }
-    println!("correct answer : {}. {}. {}", answers[0], answers[1],answers[2]);
-    println!("\nGame result\n-----------  \ns : {}\nb : {}\no : {}", sbo.strike, sbo.ball, sbo.out);
-
+    
+    sbo
 }
