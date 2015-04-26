8eb51795ea70865a83554bc334cb4737e6d1650e
Yoonsung <lvev9925@naver.com>
2015-04-26 10:26:26 +0900

    Check SBO Result from input value

--- base_ball_game/src/main.rs
+++ base_ball_game/src/main.rs
@@ -14,10 +14,14 @@ fn main() {
     println!("       숫자 야구게임       ");
     println!("========================");
 
-    let mut inputs = vec![];
+    
+    let mut inputs:Vec<i8> = vec![];
+//TODO generate random data
+    let answers:Vec<i8> = vec![1,2,3]; 
+    let mut sbo = SBO{strike:0, ball:0, out:0};
 
     loop {
-
+//TODO Modulization
         let current_input_size = inputs.len();
 
         let mut input = String::new();
@@ -25,9 +29,18 @@ fn main() {
         io::stdout().flush();
 
         io::stdin().read_line(&mut input);
-        print!("input data : {}", input);
+        let int_value: Option<i8> = input.trim().parse().ok();
+
 //TODO input data validation
-        inputs.push(input);
+        match int_value {
+            Some(value) => {
+                inputs.push(value);
+                print!("int value = {}", value);
+                io::stdout().flush();
+            },
+            None => continue
+        }
+        print!("input data : {}", input);
 
         if current_input_size == 2{
             break;
@@ -35,5 +48,33 @@ fn main() {
     }
 
     print!("빠져나왔습니다");
+
+//Check input data
+//TODO Modulization
+    let mut is_heat = false;
+    let mut answer_value: i8 = 0;
+    let mut input_value: i8 = 0;
+    for answer_index in 0..answers.len() {
+        answer_value = answers[answer_index];
+        
+        for input_index in 0..inputs.len() {
+            input_value = inputs[input_index];
+            if answer_value == input_value {
+                if answer_index == input_index {
+                    sbo.strike += 1;
+                    is_heat = true;
+                } else {
+                    sbo.ball += 1;
+                    is_heat = true;
+                } 
+            }
+        }
+        
+        if is_heat == false {
+            sbo.out += 1;
+        }
+    }
+
+    print!("s : {}, b : {}, o : {}", sbo.strike, sbo.ball, sbo.out);
 
 }
