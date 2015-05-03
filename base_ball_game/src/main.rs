4597b53839a02cf201dd42ce4018ce9e3bd9e680
Yoonsung <lvev9925@naver.com>
2015-05-03 14:28:20 +0900

    Remove warning

--- base_ball_game/src/main.rs
+++ base_ball_game/src/main.rs
@@ -3,6 +3,8 @@ use std::io::Write;
 extern crate rand;
 use rand::distributions::{IndependentSample, Range};
 
+const INNING_INPUT_LIMIT: usize= 3;
+
 struct SBO {
     strike: i8,
     ball  : i8,
@@ -15,12 +17,11 @@ fn main() {
     println!("       숫자 야구게임       ");
     println!("========================");
 
-    let mut inputs:Vec<i8> = vec![];
     let mut answers:Vec<i8> = vec![]; 
 
     let between = Range::new(1i8, 10);
-    gen_rand_array(&mut answers, 3, between); 
-    inputs = gen_array_from_user(3);
+    gen_rand_array(&mut answers, INNING_INPUT_LIMIT, between); 
+    let inputs = gen_array_from_user(INNING_INPUT_LIMIT);
 
     //Check input data
     let sbo = get_sbo_result(inputs, &answers);
@@ -30,10 +31,10 @@ fn main() {
 
 }
 
-fn gen_rand_array(arr: &mut Vec<i8>, len: i8, range: Range<i8>) {
+fn gen_rand_array(arr: &mut Vec<i8>, len: usize, range: Range<i8>) {
     let mut rng = rand::thread_rng();
 
-    while (arr.len() != 3) {
+    while arr.len() != len {
         let rand_num = range.ind_sample(&mut rng);
         if (*arr).contains(&rand_num) {
             continue;
@@ -43,15 +44,17 @@ fn gen_rand_array(arr: &mut Vec<i8>, len: i8, range: Range<i8>) {
     }
 }
 
-//TODO const number 3
+#[allow(unused_must_use)]
 fn gen_array_from_user(len: usize) -> Vec<i8> {
     let mut return_arr:Vec<i8> = vec![];
 
-    loop {
-        let current_input_size = return_arr.len();
+    let mut current_input_size:usize= 0;
+    while current_input_size != len {
+        current_input_size = return_arr.len();
 
         let mut input = String::new();
         print!("{}번째 숫자를 입력해주세요: ", current_input_size+1);
+
         io::stdout().flush();
 
         io::stdin().read_line(&mut input);
@@ -62,30 +65,21 @@ fn gen_array_from_user(len: usize) -> Vec<i8> {
                 return_arr.push(value.unwrap());
             },
             Some(_)|None => {
-                println!("only Input Between 1 to 10 number!");
+               println!("only Input Between 1 to 10 number!");
                 continue;
             }
-        }
-
-        if current_input_size == len-1 {
-            break;
         }
     }
 
     return_arr 
 }
 
-//TODO Modulization
+//TODO use closure
 fn get_sbo_result(inputs :Vec<i8>, answers :&Vec<i8>) -> SBO {
     let mut sbo = SBO{strike:0, ball:0, out:0};
     let mut is_heat = false;
-    let mut answer_value: i8 = 0;
-    let mut input_value: i8 = 0;
-    for answer_index in 0..(answers).len() {
-        answer_value = (answers)[answer_index];
-        
-        for input_index in 0..(inputs).len() {
-            input_value = (inputs)[input_index];
+    for (answer_value, answer_index) in answers.iter().enumerate() {
+        for (input_value, input_index) in inputs.iter().enumerate() {
             if answer_value == input_value {
                 if answer_index == input_index {
                     sbo.strike += 1;
